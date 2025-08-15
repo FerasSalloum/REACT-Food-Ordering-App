b89dcb596ada6f52acf6732bab4747d54e52be4e
FerasSalloum <ferassalloum1999l@gmail.com>
2025-08-15 20:04:00 +0300

    Bug Fixing

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
 
 const PlaceOrder = () => {
   return (
-    <div>
-      
+    <div className="max-padd-container mt-24">
+      <Title
+        title1={"NO"}
+        title2={"Orders"}
+        titleStyle={"h3"}
+        paraStyle={"hidden"}
+      />
     </div>
-  )
-}
- 
-export default PlaceOrder
+  );
+};
+
+export default PlaceOrder;
